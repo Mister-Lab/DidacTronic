cc762c72c362b1f7a99c5ea4d1f27282d41b3254
Misterlab <01escience@gmail.com>
2023-09-01 12:24:24 +0200

    Repair Tab course
    Svg régime TT

--- js/3_course_aside.js
+++ js/3_course_aside.js
@@ -1,10 +1,10 @@
 
 function openAsidenav() {
   document.getElementById("aside").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.getElementById("nav").style.marginLeft = "250px";
-  document.getElementById("footer").style.marginLeft = "250px";
-  document.getElementById("header").style.marginLeft = "250px";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("nav").style.marginLeft = "0";
+  document.getElementById("footer").style.marginLeft = "0";
+  document.getElementById("header").style.marginLeft = "0";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
